f23ba0a883afcf3f9c76288c07f1558ddf884860
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-01 15:21:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Omar',
+    address: 'Bunyala'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const anEmployee = {...employee};
+    anEmployee[key] = value;
+    return anEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const anEmployee = {...employee};
+    delete anEmployee[key];
+    return anEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
